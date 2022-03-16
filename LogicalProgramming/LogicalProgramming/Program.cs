ac403435da7be79ee0479dc13d15654c067e4d14
vaibhavbohari <vaibhavbohari@gmail.com>
2022-03-16 21:55:16 +0530

    Check The Prime Number

--- LogicalProgramming/LogicalProgramming/Program.cs
+++ LogicalProgramming/LogicalProgramming/Program.cs
@@ -1,20 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Welcome to Prime Number !");
-{ 
-int n, i, m = 0, sum = 0;
-Console.Write("Enter the Number to check Prime: ");
-n = Convert.ToInt32(Console.ReadLine());
-m = n / 2;
-for (i = 2; i <= m; i++)
 {
-    if (n % i == 0)
+    int n, i, m = 0, sum = 0;
+    Console.Write("Enter the Number to check Prime: ");
+    n = Convert.ToInt32(Console.ReadLine());
+    m = n / 2;
+    for (i = 2; i <= m; i++)
     {
-        Console.Write("Number is not Prime.");
-        sum = 1;
-        break;
+        if (n % i == 0)
+        {
+            Console.Write("Number is not Prime.");
+            sum = 1;
+            break;
+        }
     }
+    if (sum == 0)
+        Console.Write("Number is Prime.");
 }
-if (sum == 0)
-    Console.Write("Number is Prime.");       
-     }  
-         
