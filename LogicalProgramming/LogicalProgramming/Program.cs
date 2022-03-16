7a96ac8c96f424a6646236b3b07379242b0902c7
vaibhavbohari <vaibhavbohari@gmail.com>
2022-03-16 20:32:13 +0530

    find fibonacci Series

--- LogicalProgramming/LogicalProgramming/Program.cs
+++ LogicalProgramming/LogicalProgramming/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to Logical Programming!");
+Console.WriteLine("Welcome to Fibonacci Series !");
+
+
+    {
+        int n1 = 0,
+        n2 = 1, 
+        n3, i, 
+        number;
+        Console.Write("Enter the number of elements: ");
+       number= Convert.ToInt32(Console.ReadLine());
+        Console.Write(n1 + " " + n2 + " "); //printing 0 and 1    
+        for (i = 2; i < number; ++i) //loop starts from 2 because 0 and 1 are already printed    
+        {
+            n3 = ( n1 + n2);
+            Console.Write(n3 + " ");
+            n1 = n2;
+            n2 = n3;
+        }
+    }
