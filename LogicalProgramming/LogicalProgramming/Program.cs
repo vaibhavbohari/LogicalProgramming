de580e22573ec8f9d39356b9c4176c00ee6556c0
vaibhavbohari <vaibhavbohari@gmail.com>
2022-03-16 21:52:19 +0530

    Check The Perfect Number

--- LogicalProgramming/LogicalProgramming/Program.cs
+++ LogicalProgramming/LogicalProgramming/Program.cs
@@ -1,25 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Welcome to Prime Number !");
-
-int number, sum = 0, n;
-Console.Write("enter the Number");
-number = Convert.ToInt32(Console.ReadLine());
-n = number;
-for (int i = 1; i < number; i++)
+{ 
+int n, i, m = 0, sum = 0;
+Console.Write("Enter the Number to check Prime: ");
+n = Convert.ToInt32(Console.ReadLine());
+m = n / 2;
+for (i = 2; i <= m; i++)
 {
-    if (number % i == 0)
+    if (n % i == 0)
     {
-        sum = sum + i;
+        Console.Write("Number is not Prime.");
+        sum = 1;
+        break;
     }
 }
-if (sum == n)
-{
-    Console.WriteLine("\n Entered number is a perfect number");
-    Console.ReadLine();
-}
-else
-{
-    Console.WriteLine("\n Entered number is not a perfect number");
-    
-}
+if (sum == 0)
+    Console.Write("Number is Prime.");       
+     }  
          
