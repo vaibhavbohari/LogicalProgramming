8b720ec85e61239257a2d9f5816a20207186dae8
vaibhavbohari <vaibhavbohari@gmail.com>
2022-03-18 21:22:56 +0530

    Coupon Numbder

--- LogicalProgramming/LogicalProgramming/Program.cs
+++ LogicalProgramming/LogicalProgramming/Program.cs
@@ -1,23 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to Prime Number !");
+Console.WriteLine("Welcome to Coupan Number !");
+Random random = new Random();
 
+var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+var stringChars = new char[6];
+
+for (int i = 0; i < stringChars.Length; i++)
 {
-    int num = 0;
-    int temp = 0;
-    int remainder = 0;
-    int reverse = 0;
-    {
-        Console.WriteLine("Enter an integer \n");
-    }
-    num = Convert.ToInt32(Console.ReadLine());
-
-    temp = num;
-    while (num > 0)
-    {
-        remainder = num % 10;
-        reverse = reverse * 10 + remainder;
-        num /= 10;
-    }
-    Console.WriteLine("Given number is = {0}", temp);
-    Console.WriteLine("Its reverse is = {0}", reverse);
+    stringChars[i] = chars[random.Next(chars.Length)];
+    Console.WriteLine(" Coupon Number " + stringChars[i]);
 }
+
+
