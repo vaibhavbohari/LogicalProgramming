f802e6897f666c7f98f8b9a7c026d7b0da0e1f59
vaibhavbohari <vaibhavbohari@gmail.com>
2022-03-21 20:15:46 +0530

    Sqrt Find Value

--- LogicalProgramming/LogicalProgramming/Program.cs
+++ LogicalProgramming/LogicalProgramming/Program.cs
@@ -1,43 +1,25 @@
-﻿using System;
-
-public class VendingMachine
+﻿public class Sqrt_Newton
 {
+    public static double c, t, epsilon;
+    public static void Sqrt(double c)
+    {
+        double t = c;
+        double epsilon = 1 * Math.Pow(10, -15);
+        while (Math.Abs(t - c / t) > epsilon * t)
+        {
+            t = (t + c / t) / 2;
+        }
+        Console.WriteLine($"Square root of {c} is : {t} ");
 
-	// function to count and
-	// print currency notes
-	public static void countCurrency(int amount)
-	{
-		int[] notes = { 1000, 500, 100, 50, 10, 5, 2, 1 };
-		int[] Counter = new int[8];
-
-		// count notes 
-		for (int i = 0; i < 8; i++)
-		{
-			if (amount >= notes[i])
-			{
-				Counter[i] = amount / notes[i];
-				amount = amount - Counter[i] * notes[i];
-			}
-		}
-
-		// Print notes
-		Console.WriteLine("Currency Count ->");
-		for (int i = 0; i < 8; i++)
-		{
-			if (Counter[i] != 0)
-			{
-				Console.WriteLine(notes[i] + " : "
-					+ Counter[i]);
-			}
-		}
-	}
-
-	public static void Main()
-	{
-		Console.WriteLine("Enter Amount");
-		int amount = Convert.ToInt32(Console.ReadLine());
-		countCurrency(amount);
-	}
-
-
+    }
+}
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Sqrt_Newton s = new Sqrt_Newton();
+        Console.WriteLine("Enter a number value");
+        double c = Convert.ToDouble(Console.ReadLine());
+        Sqrt_Newton.Sqrt(c);
+    }
 }
