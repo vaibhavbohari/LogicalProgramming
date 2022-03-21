2978b2f60d142b4e78e30e6380d4cfb3bb74d75c
vaibhavbohari <vaibhavbohari@gmail.com>
2022-03-21 19:56:31 +0530

    Stopwatch Time

--- LogicalProgramming/LogicalProgramming/Program.cs
+++ LogicalProgramming/LogicalProgramming/Program.cs
@@ -1,15 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to Coupan Number !");
-Random random = new Random();
-
-var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-var stringChars = new char[6];
-
-for (int i = 0; i < stringChars.Length; i++)
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+class Program
 {
-    stringChars[i] = chars[random.Next(chars.Length)];
-    Console.WriteLine(" Coupon Number " + stringChars[i]);
+    static void Main()
+    {
+
+        // Create new stopwatch.
+        Stopwatch stopwatch = new Stopwatch();
+
+        Console.WriteLine("Starting Time ");
+        stopwatch.Start();
+
+        // Do something.
+        for (int i = 0; i < 1000; i++)
+        {
+            Thread.Sleep(5);
+        }
+
+        Console.WriteLine("Time stopped");
+        stopwatch.Stop();
+
+        // Write result.
+        Console.WriteLine("Time elapsed: {0}", stopwatch.Elapsed);
+    }
 }
 
 
